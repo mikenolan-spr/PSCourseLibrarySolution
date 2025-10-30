ba0d5c06f747f46064a356bbbceb1c02db11d040
Mike Nolan <mike.nolan@spr.com>
2025-10-30 10:22:02 -0500

    Initial set of project files

---
+++ PSCourseLibrary/Calculator.cs
@@ -0,0 +1,14 @@
+﻿namespace PSCourseLibrary;
+
+public class Calculator
+{
+    public int add(int a, int b)
+    {
+        return a + b;
+    }
+
+    public int sub(int a, int b)
+    {
+        return a - b;
+    }
+}
